2c256d9fedc7b1329dfeddd4871633e851de82a5
strkto <67833385+strkto@users.noreply.github.com>
2020-07-13 14:28:04 +0300

    Initial Commit

---
+++ Rock, paper, scissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rock, paper, scissors
+//
+//  Created by Константин Стародубцев on 13.07.2020.
+//  Copyright © 2020 Константин Стародубцев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
